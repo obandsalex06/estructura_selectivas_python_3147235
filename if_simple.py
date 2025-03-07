e5723cd6415182f59acc355e24e72a54bab4f925
dilan <dilan.velandiar@gmail.com>
2025-03-07 10:58:17 -0500

    if simple

---
+++ if_simple.py
@@ -0,0 +1,15 @@
+'''
+Sintaxis de if simple:
+if condicional :
+ instruccion1
+ instruccion2
+'''
+
+#Ejemplo:
+
+a = 9
+b = 3
+
+if a > b :
+    print("a es mayor que b")
+    print("y hoy es viernesss")
