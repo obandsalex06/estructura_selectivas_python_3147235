80d50e76517cab0aab5c92cbe9e3dbfdba0d67ff
dilan <dilan.velandiar@gmail.com>
2025-03-19 08:21:22 -0500

    motor

---
+++ actividad_1.py
@@ -0,0 +1,8 @@
+tem = 70
+if tem < 80:
+    print ("El motor esta en funcionamiento")
+    print ("puede seguir trabajando")
+else:
+    print("El motor se apagara automaticamente")
+    print("no puede seguir trabajando por alta temperatura")
+print("fin programa")
